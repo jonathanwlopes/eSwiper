a60d70cc94283562fce8774a1cf0c3d9241aea25
Jonathan Lopes <jonathanwlopes93@gmail.com>
2021-09-25 07:41:51 -0300

    refactoring

--- src/modules/eSwiper/index.js
+++ src/modules/eSwiper/index.js
@@ -1,108 +1,104 @@
-export const eSwiper = ({ principalParent, configs }) => {
-  const {
-    direction,
-    loop,
-    scrollbar,
-    arrows,
-    pagination,
-    paginationType,
-    dynamicBullets,
-    clickable,
-    slidesPerView,
-    spaceBetween,
-    rows,
-    mousewheel,
-    autoplay,
-    lazy,
-  } = configs
-
-  const _principalParent = document.querySelector(`.${principalParent}`)
-  _principalParent.classList.add("swiper")
-
-  const _wrapperSwiper = Element({ type: "div", classList: ["swiper-wrapper"] })
-  _principalParent.appendChild(_wrapperSwiper)
-
-  const _banners = _principalParent.querySelectorAll(".box-banner")
-  _banners.forEach((_banner) => {
-    _banner.classList.add("swiper-slide")
-    _wrapperSwiper.appendChild(_banner)
-
-    if (lazy) {
-      const _image = _banner.querySelector("img")
-      _image.classList.add("swiper-lazy")
-      _image.setAttribute("data-src", _image.src)
-      _image.removeAttribute("src")
-    }
-  })
-
-  const _shelfImages = _principalParent.querySelectorAll(".shelf > ul > li")
-  _shelfImages.forEach((_shelfImage) => {
-    _shelfImage.classList.add("swiper-slide")
-    _wrapperSwiper.appendChild(_shelfImage)
-
-    if (lazy) {
-      const _image = _shelfImage.querySelector("img")
-      _image.classList.add("swiper-lazy")
-      _image.setAttribute("data-src", _image.src)
-      _image.removeAttribute("src")
-    }
-  })
-
-  if (scrollbar) {
-    const _scrollbar = Element({ type: "div", classList: ["swiper-scrollbar"] })
-    _principalParent.appendChild(_scrollbar)
-  }
-
-  if (arrows) {
-    const _buttonNext = Element({ type: "div", classList: ["swiper-button-next"] })
-    const _buttonPrev = Element({ type: "div", classList: ["swiper-button-prev"] })
-    _principalParent.appendChild(_buttonNext)
-    _principalParent.appendChild(_buttonPrev)
-  }
-
-  if (pagination) {
-    const _pagination = Element({ type: "div", classList: ["swiper-pagination"] })
-    _principalParent.appendChild(_pagination)
-  }
-
+export const eSwiper = ({ configs }) => {
+  const { swiperContainer } = configs
+  const _swiperContainer = swiperStructure(swiperContainer, configs)
   eSwiperGenerate({
-    _principalParent,
-    direction,
-    loop,
-    arrows,
-    dynamicBullets,
-    pagination,
-    paginationType,
-    clickable,
-    slidesPerView,
-    spaceBetween,
-    rows,
-    mousewheel,
-    autoplay,
-    scrollbar,
-    lazy,
+    _swiperContainer,
+    configs,
   })
 }
 
-export const eSwiperGenerate = ({
-  _principalParent,
-  direction,
-  loop,
-  arrows,
-  dynamicBullets,
-  pagination,
-  paginationType = "bullets",
-  clickable,
-  slidesPerView = 1,
-  spaceBetween = 2,
-  rows = 1,
-  mousewheel,
-  autoplay,
-  scrollbar,
-  lazy,
-}) => {
-  new Swiper(_principalParent, {
-    direction: direction,
+export const swiperStructure = (swiperContainer, configs) => {
+  const { scrollbar, arrows, pagination, lazy, isZoom } = configs
+
+  const _swiperContainer = document.querySelector(`.${swiperContainer}`)
+  _swiperContainer.classList.add("swiper")
+
+  const _wrapperSwiper = Element({ type: "div", classList: ["swiper-wrapper"] })
+
+  const desktopWidth = screen.width >= 1024
+
+  const _slides = [..._swiperContainer.children]
+  _slides.forEach((_slide) => {
+    const _image = _slide.querySelector("img")
+    _slide.classList.add("swiper-slide")
+    _wrapperSwiper.appendChild(_slide)
+
+    if (isZoom && desktopWidth) {
+      const _wrapperZoom = Element({ type: "div", classList: ["wrapper-zoom"] })
+      _slide.appendChild(_wrapperZoom)
+      _wrapperZoom.appendChild(_image)
+    }
+
+    if (lazy) {
+      const _preLoader = Element({
+        type: "div",
+        classList: ["swiper-lazy-preloader"],
+      })
+      _slide.appendChild(_preLoader)
+      _image.classList.add("swiper-lazy")
+      _image.setAttribute("data-src", _image.src)
+      _image.removeAttribute("src")
+    }
+  })
+
+  _swiperContainer.appendChild(_wrapperSwiper)
+
+  if (isZoom && desktopWidth) $(".wrapper-zoom").zoom()
+
+  if (scrollbar) {
+    const _scrollbar = Element({ type: "div", classList: ["swiper-scrollbar"] })
+    _swiperContainer.appendChild(_scrollbar)
+  }
+
+  if (arrows) {
+    const _buttonNext = Element({
+      type: "div",
+      classList: ["swiper-button-next"],
+    })
+    const _buttonPrev = Element({
+      type: "div",
+      classList: ["swiper-button-prev"],
+    })
+    _swiperContainer.appendChild(_buttonNext)
+    _swiperContainer.appendChild(_buttonPrev)
+  }
+
+  if (pagination) {
+    const _pagination = Element({
+      type: "div",
+      classList: ["swiper-pagination"],
+    })
+    _swiperContainer.appendChild(_pagination)
+  }
+
+  return _swiperContainer
+}
+
+export const eSwiperGenerate = ({ _swiperContainer, configs }) => {
+  const {
+    direction,
+    loop,
+    arrows,
+    dynamicBullets,
+    pagination,
+    paginationType = "bullets",
+    clickable,
+    slidesPerView = 1,
+    spaceBetween = 2,
+    rows = 1,
+    mousewheel,
+    autoplay,
+    scrollbar,
+    lazy,
+    thumbs,
+    slideToClickedSlide,
+  } = configs
+
+  let swiper = new Swiper(_swiperContainer, {
+    zoom: {
+      maxRatio: 2,
+    },
+    direction,
     loop: loop,
     slidesPerView,
     spaceBetween,
@@ -112,6 +108,7 @@ export const eSwiperGenerate = ({
     mousewheel,
     autoplay,
     lazy,
+    slideToClickedSlide,
 
     pagination: {
       el: pagination ? ".swiper-pagination" : "",
@@ -130,6 +127,80 @@ export const eSwiperGenerate = ({
       el: scrollbar ? ".swiper-scrollbar" : "",
     },
   })
+
+  if (thumbs) thumbsGenerate(swiper, configs)
+}
+
+export const thumbsGenerate = (swiper, configs) => {
+  const { thumbsConfigs, lazy } = configs
+  const { slidesPerView = 1, spaceBetween = 2, direction, loop, arrows, slideToClickedSlide } = thumbsConfigs
+
+  console.log(slideToClickedSlide)
+  const _thumbsContainer = thumbsStructure(configs)
+
+  let swiperThumbs = new Swiper(_thumbsContainer, {
+    direction,
+    loop,
+    arrows,
+    spaceBetween,
+    lazy,
+    slidesPerView,
+    slideToClickedSlide,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    thumbs: {
+      swiper,
+    },
+  })
+}
+
+export const thumbsStructure = (configs) => {
+  const { thumbsConfigs, lazy } = configs
+  const { thumbsContainer, arrows } = thumbsConfigs
+
+  const _thumbsContainer = document.querySelector(`.${thumbsContainer}`)
+  _thumbsContainer.classList.add("swiper")
+  _thumbsContainer.setAttribute("thumbsSlider", "")
+
+  const _wrapperSwiper = Element({ type: "div", classList: ["swiper-wrapper"] })
+
+  const _slides = [..._thumbsContainer.children]
+  _slides.forEach((_slide) => {
+    _slide.classList.add("swiper-slide")
+    _wrapperSwiper.appendChild(_slide)
+
+    if (lazy) {
+      const _preLoader = Element({
+        type: "div",
+        classList: ["swiper-lazy-preloader"],
+      })
+      _slide.appendChild(_preLoader)
+      const _image = _slide.querySelector("img")
+      _image.classList.add("swiper-lazy")
+      _image.setAttribute("data-src", _image.src)
+      _image.removeAttribute("src")
+    }
+  })
+
+  _thumbsContainer.appendChild(_wrapperSwiper)
+
+  if (arrows) {
+    const _buttonNext = Element({
+      type: "div",
+      classList: ["swiper-button-next"],
+    })
+    const _buttonPrev = Element({
+      type: "div",
+      classList: ["swiper-button-prev"],
+    })
+    _thumbsContainer.appendChild(_buttonNext)
+    _thumbsContainer.appendChild(_buttonPrev)
+  }
+
+  return _thumbsContainer
 }
 
 export const Element = ({ type, classList }) => {
